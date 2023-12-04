5cfef87a30209e36fe336ad0f9ee7e87f422c123
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2023-12-04 16:09:17 -0500

    mejora campos de texto en login

--- app/src/main/java/com/example/inventoryapp/view/Login.kt
+++ app/src/main/java/com/example/inventoryapp/view/Login.kt
@@ -2,11 +2,20 @@ package com.example.inventoryapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import android.widget.CompoundButton
 import com.example.inventoryapp.R
+import com.example.inventoryapp.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
