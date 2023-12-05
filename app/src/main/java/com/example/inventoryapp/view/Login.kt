e030a70be520ed1961531b3f616470a1193c54ac
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2023-12-04 19:57:59 -0500

    parte logica (sin guardar en firebase) del login completamente terminada

--- app/src/main/java/com/example/inventoryapp/view/Login.kt
+++ app/src/main/java/com/example/inventoryapp/view/Login.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
-import androidx.core.content.ContextCompat
-import androidx.core.widget.doOnTextChanged
+
 import com.example.inventoryapp.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
@@ -15,32 +13,43 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //se utiliza viewbinding
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.passwordText.doOnTextChanged { text, start, before, count ->
-            if (text!!.length<6){
-                binding.passLayout.error = "La contraseña debe ser mayor a 6 caracteres"
-            }else if(text!!.length>10){
-                binding.passLayout.error = "La contraseña debe ser menor a 10 caracteres"
-            }else{
-                binding.passLayout.error = null
-            }
-        }
+        //bindings y listeners
         binding.emailText.addTextChangedListener(TextWatcher)
         binding.passwordText.addTextChangedListener(TextWatcher)
+        binding.btnLogin.setOnClickListener{
+        }
+        binding.registro.setOnClickListener {
+        }
 
     }
+    //Variable textWatcher que obtiene el estado en vivo de cada InputText para realizar acciones
     private var TextWatcher = object : TextWatcher{
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
         }
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //If para medir longitud de contraseña
+            if (binding.passwordText.text!!.length<6){
+                binding.passLayout.error = "La contraseña debe ser mayor a 6 caracteres"
+            }else if(binding.passwordText.text!!.length>10){
+                binding.passLayout.error = "La contraseña debe ser menor a 10 caracteres"
+            }else{
+                binding.passLayout.error = null
+            }
+
+            //parte encargada de habilitar botones de login y registro
             val email = binding.emailText.text.toString().trim()
             val pass = binding.passwordText.text.toString().trim()
             binding.btnLogin.isEnabled = email.isNotEmpty() && pass.isNotEmpty() && binding.passLayout.error==null
+            binding.registro.isEnabled = email.isNotEmpty() && pass.isNotEmpty() && binding.passLayout.error==null
         }
         override fun afterTextChanged(s: Editable?) {
         }
     }
+
+
 }
 
