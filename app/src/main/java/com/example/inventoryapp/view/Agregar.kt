e2d24c98f958440c1da95c3fd107e451f2c80635
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2023-12-08 12:01:02 -0500

    disponibilidad de boton guardar basado en campos de texto

--- app/src/main/java/com/example/inventoryapp/view/Agregar.kt
+++ app/src/main/java/com/example/inventoryapp/view/Agregar.kt
@@ -1,8 +1,12 @@
 package com.example.inventoryapp.view
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.OnBackPressedCallback
 import com.example.inventoryapp.R
 import com.example.inventoryapp.databinding.ActivityAgregarBinding
@@ -36,7 +40,26 @@ class Agregar : AppCompatActivity() {
 
         })
 
+        //bindings y listeners
+        binding.codigoText.addTextChangedListener(TextWatcher)
+        binding.precioTexto.addTextChangedListener(TextWatcher)
+        binding.cantidadText.addTextChangedListener(TextWatcher)
+        binding.articuloText.addTextChangedListener(TextWatcher)
 
-
+    }
+    //Variable textWatcher que obtiene el estado en vivo de cada InputText para realizar acciones
+    private var TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //parte encargada de habilitar botones de login y registro
+            val codigo = binding.codigoText.text.toString().trim()
+            val nombre = binding.articuloText.text.toString().trim()
+            val precio = binding.precioTexto.text.toString().trim()
+            val cantidad = binding.cantidadText.text.toString().trim()
+            binding.btnGuardar.isEnabled = codigo.isNotEmpty() && nombre.isNotEmpty() && precio.isNotEmpty() && cantidad.isNotEmpty()
+        }
+        override fun afterTextChanged(s: Editable?) {
+        }
     }
 }
